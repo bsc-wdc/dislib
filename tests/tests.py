1987fbd5a1cda852ea1db432a5dcba678fa3d570
Pol Alvarez Vecino <pol.avms@gmail.com>
2018-10-22 19:27:31 +0200

    added: gitignore, removed: unused tests
    Signed-off-by: Pol Alvarez Vecino <pol.avms@gmail.com>

--- tests/tests.py
+++ tests/tests.py
@@ -13,8 +13,6 @@ class ImportTests(unittest.TestCase):
     def test_import_cascadecsvm(self):
         from pycompss_lib.ml.classification import CascadeSVM
 
-    def test_import_random_forest(self):
-        from pycompss_lib.ml.classification import RandomForestClassifier
 
     def test_import_kmeans(self):
         from pycompss_lib.ml.clustering import kmeans
@@ -22,30 +20,6 @@ class ImportTests(unittest.TestCase):
     def test_import_dbscan(self):
         from pycompss_lib.ml.clustering import DBSCAN
 
-    def test_import_pca(self):
-        from pycompss_lib.ml.analysis import pca
-
-    def test_import_cholesky(self):
-        from pycompss_lib.math.linalg import cholesky
-
-    def test_import_matmul(self):
-        from pycompss_lib.math.linalg import matmul
-        from pycompss_lib.math.linalg.matmul import dot, initialize_variables
-
-    def test_import_qr(self):
-        from pycompss_lib.math.linalg import qr
-
-    def test_import_max_norm(self):
-        from pycompss_lib.algorithms import max_norm
-
-    def test_import_terasort(self):
-        from pycompss_lib.algorithms import terasort
-
-    def test_import_sort(self):
-        from pycompss_lib.algorithms import sort
-
-    def test_import_sort_by_key(self):
-        from pycompss_lib.algorithms import sort_by_key
 
 
 class ResultsTest(unittest.TestCase):
@@ -55,16 +29,6 @@ class ResultsTest(unittest.TestCase):
 
     def test_cascadecsvm(self):
         from pycompss_lib.ml.classification import CascadeSVM
-
-    def test_max_norm(self):
-        from pycompss_lib.algorithms import max_norm
-        
-        points, dimensions, fragments, seed = 16000, 3, 16, 666
-        expected_output = 16410.464761528907
-
-        result = max_norm(points, dimensions, fragments, seed)
-
-        self.assertEqual(result, expected_output)
 
     def test_fft(self):
         import numpy as np
