f75eef4a8604688be204069355acce1940cd76ab
Javier Alvarez <javier.alvarez@bsc.es>
2018-10-23 14:56:27 +0200

    Updated docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,6 +42,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
+    'numpydoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
