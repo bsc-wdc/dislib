05fc80140d9f6156b8698a74864919b0cfdf136e
Pol Alvarez Vecino <pol.avms@gmail.com>
2018-11-14 10:52:10 +0100

    test: attempting docker build & test
    Signed-off-by: Pol Alvarez Vecino <pol.avms@gmail.com>

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,16 @@
 export ComputingUnits=4
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
+export MPI_HOME=/usr/lib64/openmpi
+export LD_LIBRARY_PATH=/usr/lib64/openmpi/lib
+export PATH=$PATH:/opt/COMPSs/Runtime/scripts/user
+export CLASSPATH=$CLASSPATH:/opt/COMPSs/Runtime/compss-engine.jar
+export PATH=$PATH:/opt/COMPSs/Bindings/c/bin
+
+/etc/init.d/ssh start
+
+coverage run --source dislib tests/tests.py
+coverage report -m
+
 runcompss \
-    tests/tests.py
+    --debug \
+    /home/dislib/tests/tests.py
