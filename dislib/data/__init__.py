ba9171d3bf38298510f6312aa59c50b85b48f3b6
Pol Alvarez Vecino <pol.avms@gmail.com>
2018-11-23 12:02:53 +0100

    mend
    Signed-off-by: Pol Alvarez Vecino <pol.avms@gmail.com>

--- dislib/data/__init__.py
+++ dislib/data/__init__.py
@@ -1,6 +1,6 @@
+from dislib.data.classes import Subset, Dataset
 from dislib.data.base import load_data, load_libsvm_file, load_libsvm_files, \
     load_csv_file, load_csv_files
-from dislib.data.classes import Subset, Dataset
 
 __all__ = [Dataset, Subset, load_data, load_libsvm_file, load_libsvm_files,
            load_csv_file, load_csv_files]
--- run_coverage.sh
+++ run_coverage.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+# We make sure that ssh daemon is running
 /etc/init.d/ssh start
 
-coverage run --source dislib tests/tests.py
-coverage report -m
+coverage3 run --source dislib tests/tests.py
+coverage3 report -m
 
 bash <(curl -s https://codecov.io/bash)
