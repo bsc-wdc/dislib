06fe6219bbd7d28dabe68c47715ba4055c99b182
Pol Alvarez Vecino <pol.avms@gmail.com>
2018-11-23 13:01:25 +0100

    added: mock import pycompss
    Signed-off-by: Pol Alvarez Vecino <pol.avms@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,6 +40,8 @@ napoleon_google_docstring = False
 napoleon_use_param = False
 napoleon_use_ivar = True
 
+autodoc_mock_imports = ['pycompss']
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
