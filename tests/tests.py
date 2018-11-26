c61ecadc3ae7c888a00d86ed413c5b5314d025c6
Javier Alvarez <javier.alvarez@bsc.es>
2018-11-26 20:46:09 +0100

    More tests

--- tests/tests.py
+++ tests/tests.py
@@ -108,16 +108,15 @@ class DBSCANTest(unittest.TestCase):
         self.assertEqual(n_clusters, 4)
 
 
-class DataTest(unittest.TestCase):
+class DataLoadingTest(unittest.TestCase):
 
-    def test_load_data(self):
+    def test_load_data_with_labels(self):
         from sklearn.datasets import make_blobs
         from dislib.data import load_data
         import numpy as np
 
         x, y = make_blobs(n_samples=1500)
         data = load_data(x=x, y=y, subset_size=100)
-        data.collect()
 
         read_x = np.empty((0, x.shape[1]))
         read_y = np.empty(0)
@@ -128,6 +127,24 @@ class DataTest(unittest.TestCase):
 
         self.assertTrue((read_x == x).all())
         self.assertTrue((read_y == y).all())
+        self.assertEqual(len(data), 15)
+
+    def test_load_data_without_labels(self):
+        from sklearn.datasets import make_blobs
+        from dislib.data import load_data
+        import numpy as np
+
+        x, y = make_blobs(n_samples=1500)
+        data = load_data(x=x, subset_size=100)
+
+        read_x = np.empty((0, x.shape[1]))
+        read_y = np.empty(0)
+
+        for subset in data:
+            read_x = np.concatenate((read_x, subset.samples))
+            read_y = np.concatenate((read_y, subset.labels))
+
+        self.assertTrue((read_x == x).all())
         self.assertEqual(len(data), 15)
 
     def test_load_libsvm_file_sparse(self):
@@ -253,8 +270,8 @@ class DataTest(unittest.TestCase):
         from dislib.data import load_csv_file
         import numpy as np
 
-        csv_file = "./tests/files/csv/1"
-        data = load_csv_file(csv_file, subset_size=1000, n_features=121,
+        csv_file = "./tests/files/csv/2"
+        data = load_csv_file(csv_file, subset_size=100, n_features=121,
                              label_col="first")
         data.collect()
         csv = np.loadtxt(csv_file, delimiter=",")
@@ -268,25 +285,119 @@ class DataTest(unittest.TestCase):
 
         self.assertTrue((read_x == csv[:, 1:]).all())
         self.assertTrue((read_y == csv[:, 0]).all())
-        self.assertEqual(len(data), 5)
+        self.assertEqual(len(data), 44)
 
     def test_load_csv_files(self):
         from dislib.data import load_csv_files
         import numpy as np
+        import os
 
         csv_dir = "./tests/files/csv"
+        file_list = os.listdir(csv_dir)
         data = load_csv_files(csv_dir, n_features=122)
         data.collect()
-        csv = np.loadtxt(csv_dir, delimiter=",")
 
-        read_x = np.empty((0, csv.shape[1]))
+        for i, subset in enumerate(data):
+            csv_file = os.path.join(csv_dir, file_list[i])
+            csv = np.loadtxt(csv_file, delimiter=",")
 
-        for subset in data:
-            read_x = np.concatenate((read_x, subset.samples))
+            self.assertTrue((subset.samples == csv).all())
 
-        self.assertTrue((read_x == csv).all())
-        self.assertEqual(len(data), 15)
-        self.assertIsNone(subset.labels)
+        self.assertEqual(len(data), 3)
+
+    def test_load_csv_files_labels_last(self):
+        from dislib.data import load_csv_files
+        import numpy as np
+        import os
+
+        csv_dir = "./tests/files/csv"
+        file_list = os.listdir(csv_dir)
+        data = load_csv_files(csv_dir, n_features=122, label_col="last")
+        data.collect()
+
+        for i, subset in enumerate(data):
+            csv_file = os.path.join(csv_dir, file_list[i])
+            csv = np.loadtxt(csv_file, delimiter=",")
+
+            self.assertTrue((subset.samples == csv[:, :-1]).all())
+            self.assertTrue((subset.labels == csv[:, -1]).all())
+
+        self.assertEqual(len(data), 3)
+
+    def test_load_csv_files_labels_first(self):
+        from dislib.data import load_csv_files
+        import numpy as np
+        import os
+
+        csv_dir = "./tests/files/csv"
+        file_list = os.listdir(csv_dir)
+        data = load_csv_files(csv_dir, n_features=122, label_col="first")
+        data.collect()
+
+        for i, subset in enumerate(data):
+            csv_file = os.path.join(csv_dir, file_list[i])
+            csv = np.loadtxt(csv_file, delimiter=",")
+
+            self.assertTrue((subset.samples == csv[:, 1:]).all())
+            self.assertTrue((subset.labels == csv[:, 0]).all())
+
+        self.assertEqual(len(data), 3)
+
+
+class DataClassesTest(unittest.TestCase):
+
+    def test_dataset_get_item(self):
+        from dislib.data import load_data
+        import numpy as np
+
+        arr = np.array((range(10), range(10, 20)))
+        dataset = load_data(arr, subset_size=2)
+        samples = dataset[0].samples
+
+        self.assertTrue((samples[0] == arr[0]).all())
+
+    def test_dataset_len(self):
+        from dislib.data import load_data
+        import numpy as np
+
+        arr = np.zeros((40, 25))
+        dataset = load_data(arr, subset_size=5)
+
+        self.assertEqual(len(dataset), 8)
+
+    def test_dataset_append(self):
+        from dislib.data import Subset, load_data
+        import numpy as np
+
+        arr = np.zeros((40, 25))
+        dataset = load_data(arr, subset_size=5)
+        subset = Subset(samples=arr)
+        dataset.append(subset)
+
+        self.assertEqual(len(dataset), 9)
+
+    def test_dataset_extend(self):
+        from dislib.data import Subset, load_data
+        import numpy as np
+
+        arr = np.zeros((40, 25))
+        dataset = load_data(arr, subset_size=5)
+        subset1 = Subset(samples=np.zeros((20, 18)))
+        subset2 = Subset(samples=np.zeros((24, 18)))
+        dataset.extend(subset1, subset2)
+
+        self.assertEqual(len(dataset), 10)
+
+    def test_dataset_collect(self):
+        from dislib.data import load_csv_file, Subset
+
+        csv_file = "./tests/files/csv/3"
+        dataset = load_csv_file(csv_file, subset_size=300, n_features=122)
+        dataset.collect()
+
+        self.assertIsInstance(dataset[0], Subset)
+
+
 
 
 def main():
