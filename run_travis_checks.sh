7f1f27afbf88597a52759d5e4ffe4f4fde52efbb
Pol Alvarez Vecino <pol.avms@gmail.com>
2018-12-10 12:04:29 +0100

    reordered travis checks
    Signed-off-by: Pol Alvarez Vecino <pol.avms@gmail.com>

--- run_travis_checks.sh
+++ run_travis_checks.sh
@@ -1,6 +1,13 @@
+#!/bin/bash -e
+
 root_path="$(dirname "$(readlink -f "$0")")"
 cd ${root_path}
 
-./run_tests.sh
-./run_coverage.sh
+echo "Running flake8 style check"
 ./run_style.sh
+
+echo "Running tests"
+./run_tests.sh
+
+echo "Running code coverage"
+./run_coverage.sh
