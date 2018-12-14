b896e31dc2a1db311b7ccaf823dacc008b76baf9
Pol Alvarez Vecino <pol.avms@gmail.com>
2018-12-14 15:46:25 +0100

    added: support for multiple test files
    Signed-off-by: Pol Alvarez Vecino <pol.avms@gmail.com>

--- run_tests.sh
+++ run_tests.sh
@@ -8,15 +8,28 @@ cd ${root_path}
 # Default process per worker
 export ComputingUnits=4
 
-runcompss \
-    --pythonpath=$(pwd) \
-    --python_interpreter=python3 \
-    ./tests/tests.py &> >(tee output.log)
 
-result=$(cat output.log | egrep "OK|FAILED")
+for test_path in $(ls ./tests/*py); do
+    test_file=$(basename -- "${test_path}")
+    # We don't want to run the __init__.py file
+    if [ "${test_file}" == "__init__.py" ]; then
+        continue
+    fi
+    runcompss \
+        --pythonpath=$(pwd) \
+        --python_interpreter=python3 \
+        ./tests/${test_file} > >(tee -a ${test_file}.log) 2> >(tee -a ${test_file}.log >&2)
 
-echo "Tests result: ${result}"
+    # (1) Script fails fast (-e) so at this point testing will stop if runcompss returns 1
 
-if [[ $result =~ FAILED ]]; then 
-    exit 1
-fi
+    # Check the unittest result
+    result=$(cat ${test_file}.log | egrep "OK|FAILED")
+
+    # Print the unittest result
+    echo "[${test_file}] Test result: ${result}"
+
+    # If the unittest fail the testing is stopped to mirror the (-e) behaviour in (1)
+    if [[ $result =~ FAILED ]]; then 
+        exit 1
+    fi
+done
