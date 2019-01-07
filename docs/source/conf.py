3869a495aaff7bb97920ddcbd8bdc645907fe42b
Javier Alvarez <javier.alvarez@bsc.es>
2019-01-07 17:38:36 +0100

    conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -341,8 +341,8 @@ def linkcode_resolve(domain, info):
     else:
         linespec = ""
 
-    return "http://github.com/bsc-wdc/dislib/blob/master/%s/%s%s" \
-           % (modname.replace(".", "/"), fullname.replace(".", "/"), linespec)
+    return "http://github.com/bsc-wdc/dislib/blob/master/%s.py%s" \
+           % (modname.replace(".", "/"), linespec)
 
 
 # -- Options for Texinfo output -------------------------------------------
