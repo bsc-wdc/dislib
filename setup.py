8a76bd5dafb16ff9b457d384e306a7e884b3e95c
Javier Alvarez <javier.alvarez@bsc.es>
2019-01-29 11:47:29 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="dislib",
+    version="0.1.1",
+    author="Barcelona Supercomputing Center",
+    author_email="javier.alvarez@bsc.es",
+    description="A distributed computing library on top of PyCOMPSs",
+    url="http://dislib.bsc.es",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: Apache Software License",
+        "Development Status :: 2 - Pre-Alpha",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Distributed Computing",
+    ],
+)
