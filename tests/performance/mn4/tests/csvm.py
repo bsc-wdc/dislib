f5ccdafc7e384ca6caa0d14a8ef9058d9845e3b8
Javier Alvarez <javier.alvarez@bsc.es>
2019-05-30 14:04:16 +0200

    Fix

--- tests/performance/mn4/tests/csvm.py
+++ tests/performance/mn4/tests/csvm.py
@@ -8,14 +8,14 @@ def main():
     kdd = load_txt_files(
         "/gpfs/projects/bsc19/COMPSs_DATASETS/dislib/kdd99/partitions/384",
         n_features=121, label_col="last")
-    covtype = load_libsvm_file(
-        "/gpfs/projects/bsc19/COMPSs_DATASETS/dislib/covtype/train",
-        subset_size=5000, n_features=54)
+    ijcnn1 = load_libsvm_file(
+        "/gpfs/projects/bsc19/COMPSs_DATASETS/dislib/ijcnn1/train",
+        subset_size=5000, n_features=22)
 
     csvm = CascadeSVM(c=10000, gamma=0.01)
 
     performance.measure("CSVM", "KDD99", csvm, kdd)
-    performance.measure("CSVM", "covtype", csvm, covtype)
+    performance.measure("CSVM", "ijcnn1", csvm, ijcnn1)
 
 
 if __name__ == "__main__":
--- tests/performance/mn4/tests/rf.py
+++ tests/performance/mn4/tests/rf.py
@@ -8,15 +8,15 @@ def main():
     kdd = load_txt_files(
         "/gpfs/projects/bsc19/COMPSs_DATASETS/dislib/kdd99/partitions/384",
         n_features=121, label_col="last")
-    covtype = load_libsvm_file(
-        "/gpfs/projects/bsc19/COMPSs_DATASETS/dislib/covtype/train",
-        subset_size=5000, n_features=54, store_sparse=False)
+    mnist = load_libsvm_file(
+        "/gpfs/projects/bsc19/COMPSs_DATASETS/dislib/mnist/train.scaled",
+        subset_size=5000, n_features=780, store_sparse=False)
 
     rf = RandomForestClassifier(n_estimators=100, distr_depth=2)
     performance.measure("RF", "KDD99", rf, kdd)
 
     rf = RandomForestClassifier(n_estimators=100, distr_depth=2)
-    performance.measure("RF", "covtype", rf, covtype)
+    performance.measure("RF", "mnist", rf, mnist)
 
 
 if __name__ == "__main__":
