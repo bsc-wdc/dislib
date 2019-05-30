8a41fdc65dff3529765ac44c5e386bffdb1e1651
Javier Alvarez <javier.alvarez@bsc.es>
2019-05-30 10:11:50 +0200

    MN4 scripts

---
+++ tests/performance/mn4/launch.py
@@ -0,0 +1,36 @@
+import subprocess
+from subprocess import PIPE
+import os
+import datetime
+
+tests_dir = "/gpfs/projects/bsc19/PERFORMANCE/dislib/tests"
+
+def main():
+    cmd = "enqueue_compss --qos=debug --exec_time=30 --pythonpath=/gpfs/projects/bsc19/PERFORMANCE/dislib/scripts:/gpfs/projects/bsc19/PERFORMANCE/dislib/tests --lang=python --worker_in_master_cpus=0 --max_tasks_per_node=48 --num_nodes=9 --master_working_dir=/gpfs/projects/bsc19/PERFORMANCE/dislib/logs".split(" ")
+    
+    out = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M")    
+    logdir = os.path.join("/gpfs/projects/bsc19/PERFORMANCE/dislib/logs/", out)
+    os.mkdir(logdir)
+    cmd.append("--master_working_dir=" + logdir)
+
+    gpfs_cmd = cmd + ["--worker_working_dir=/gpfs/scratch/bsc19/compss/COMPSs_Sandbox"]
+    scratch_cmd = cmd + ["--worker_working_dir=scratch"]
+    dependencies = "afterany"
+
+    for f in os.listdir(tests_dir):
+        if f.endswith(".py"):
+            script_path = os.path.join(tests_dir, f)
+            
+            print("Submitting " + f)
+
+            proc = subprocess.run(args=gpfs_cmd + [script_path], stdout=PIPE)
+            job_id = str(proc.stdout).split(" ")[-1][:-3]
+
+            dependencies = dependencies + ":" + job_id
+    
+    final_cmd = ["sbatch", "-n1", "--dependency=" + dependencies, "/gpfs/projects/bsc19/PERFORMANCE/dislib/scripts/postprocess.sh", out]
+    subprocess.run(final_cmd)
+
+
+if __name__ == "__main__":
+    main()
