06707b03df821f75c6a34c037da198f633fda690
Javier Alvarez <javier.alvarez@bsc.es>
2019-05-30 10:57:54 +0200

    Fixed style

--- tests/performance/mn4/scripts/performance.py
+++ tests/performance/mn4/scripts/performance.py
@@ -1,7 +1,9 @@
 import time
+
 from pycompss.api.api import compss_barrier
 
-def measure(name, model, dataset):    
+
+def measure(name, model, dataset):
     print("==== STARTING ====", name)
     s_time = time.time()
     model.fit(dataset)
--- tests/performance/mn4/scripts/postprocess.py
+++ tests/performance/mn4/scripts/postprocess.py
@@ -1,10 +1,11 @@
 import os
 import sys
-import datetime
 
-def main():      
+
+def main():
     out = "/gpfs/projects/bsc19/PERFORMANCE/dislib/results/" + sys.argv[1]
-    log_dir = os.path.join("/gpfs/projects/bsc19/PERFORMANCE/dislib/logs", sys.argv[1])
+    log_dir = os.path.join("/gpfs/projects/bsc19/PERFORMANCE/dislib/logs",
+                           sys.argv[1])
 
     for f in os.listdir(log_dir):
         if f.endswith(".out"):
@@ -15,11 +16,10 @@ def main():
                     test_name = str(line.split(" ")[-1]).rstrip()
                 elif "==== OUTPUT ====" in line:
                     time = float(line.split(" ")[-1])
-                    
+
             with open(out, "a") as fout:
                 fout.write(test_name + " " + str(time) + "\n")
-        
+
 
 if __name__ == "__main__":
     main()
-
