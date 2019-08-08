f015830f453f6bf7ebeeff915a689f5ba514a1e3
Javier Alvarez <javier.alvarez@bsc.es>
2019-08-08 12:31:12 +0200

    Added apply_along_axis

--- dislib/data/__init__.py
+++ dislib/data/__init__.py
@@ -1,9 +1,9 @@
 from dislib.data.classes import Subset, Dataset
+from dislib.data.array import array, load_svmlight_file, random_array, \
+    apply_along_axis
 from dislib.data.base import load_data, load_libsvm_file, load_libsvm_files, \
     load_txt_file, load_txt_files
 
-from dislib.data.array import array, load_svmlight_file, random_array
-
 __all__ = ['random_array', 'array', 'Dataset', 'Subset', 'load_data',
-           'load_svmlight_file',
-           'load_libsvm_files', 'load_txt_file', 'load_txt_files']
+           'load_svmlight_file', 'load_libsvm_files', 'load_txt_file',
+           'load_txt_files', 'apply_along_axis']
