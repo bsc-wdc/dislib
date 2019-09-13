39474556f2971f89ab354aca6400489e5f7cbf52
Javier Alvarez <javier.alvarez@bsc.es>
2019-09-13 09:31:01 +0200

    Added time

--- tests/performance/mn4/scripts/postprocess.sh
+++ tests/performance/mn4/scripts/postprocess.sh
@@ -4,6 +4,7 @@
 #SBATCH --workdir=.
 #SBATCH -o /gpfs/projects/bsc19/PERFORMANCE/dislib/logs/post-%J.out
 #SBATCH -e /gpfs/projects/bsc19/PERFORMANCE/dislib/logs/post-%J.err
+#SBATCH -t 00:05:00
 
 
 python /gpfs/projects/bsc19/PERFORMANCE/dislib/scripts/postprocess.py $1
