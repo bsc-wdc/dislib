40dab6646ee0134f8dd28f07c43cce6177f4181a
Adrián Espejo Saldaña <adri.espejo@gmail.com>
2020-01-22 13:20:55 +0100

    trying to fix travis

--- launch_cassandra.sh
+++ launch_cassandra.sh
@@ -3,8 +3,6 @@ docker network create --driver bridge cassandra_bridge
 CASSANDRA_ID=$(docker run --rm --network=cassandra_bridge -d cassandra)
 sleep 30
 CASSANDRA_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "${CASSANDRA_ID}")
-# connect dislib container to Cassandra container
-docker network connect cassandra_bridge dislib
 # add environment variable CONTACT_NAMES needed by Hecuba
 export CONTACT_NAMES=$CASSANDRA_IP
 echo "Using Cassandra host: $CONTACT_NAMES"
