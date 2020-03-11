17b80de635ffa11a1dccf608c2c08b9f38484ba3
mbmiquel <mbmiquel@gmail.com>
2020-03-11 13:57:01 +0100

    test

--- dislib/cluster/kmeans/base.py
+++ dislib/cluster/kmeans/base.py
@@ -94,7 +94,7 @@ class KMeans(BaseEstimator):
         while not self._converged(old_centers, iteration):
             old_centers = self.centers.copy()
             partials = []
-            for t in x._iterator:
+            for t in iter(x):
                 print(t)
             for row in x._iterator(axis=0):
                 partial = _partial_sum(row._blocks, old_centers)
