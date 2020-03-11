31de2415b48a176601ff360eaea7fbe643ff0152
mbmiquel <mbmiquel@gmail.com>
2020-03-11 13:15:11 +0100

    test

--- tests/test_test.py
+++ tests/test_test.py
@@ -59,11 +59,11 @@ x_train_hecuba.make_persistent(name="hecuba_dislib.test_array")
 print(x_train)
 print(x_train_hecuba)
 
-#kmeans = KMeans(n_clusters=3, random_state=170)
-#labels = kmeans.fit_predict(x_train).collect()
+kmeans = KMeans(n_clusters=3, random_state=170)
+labels = kmeans.fit_predict(x_train).collect()
 
-#kmeans2 = KMeans(n_clusters=3, random_state=170)
-#h_labels = kmeans2.fit_predict(l).collect()
+kmeans2 = KMeans(n_clusters=3, random_state=170)
+h_labels = kmeans2.fit_predict(x_train_hecuba).collect()
 
 #self.assertTrue(np.allclose(kmeans.centers, kmeans2.centers))
 #self.assertTrue(np.allclose(labels, h_labels))
