0ee9c27503c2a1d2e4549566e442fa57307d79b6
mbmiquel <mbmiquel@gmail.com>
2020-03-17 12:27:00 +0100

    test

--- tests/test_hecuba.py
+++ tests/test_hecuba.py
@@ -198,7 +198,8 @@ class HecubaTest(unittest.TestCase):
 
         #kmeans = KMeans(n_clusters=3, random_state=170)
         #labels = kmeans.fit_predict(x_train).collect()
-
+        print("tipo de dato")
+        print(x_train_hecuba)
         kmeans2 = KMeans(n_clusters=3, random_state=170)
         h_labels = kmeans2.fit_predict(x_train_hecuba).collect()
 
