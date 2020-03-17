f2e8a10b4fd57117538a5b2978155a44d3c914d0
mbmiquel <mbmiquel@gmail.com>
2020-03-17 15:45:26 +0100

    test

--- dislib/data/array.py
+++ dislib/data/array.py
@@ -159,6 +159,7 @@ class Array(object):
         sparse = None
         print("merge")
         print(blocks[0].__class__.__name__)
+        print(blocks)
         if blocks[0].__class__.__name__ == "StorageNumpy":
             print("entro")
             b0 = blocks[0]
