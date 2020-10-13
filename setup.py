058f4e3b2e9fa120adc63c18da30aef6c2ea73fb
Salvi Solà Martinell <salvi.solamartinell@bsc.es>
2020-10-13 23:43:44 +0200

    PyPI description shortened

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 import setuptools
 
 
-# read the contents of the README.md file
+# read the contents of the README.md file to get a long_description
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+    readme = f.read()
+    long_description = readme[:readme.find('## Contents')].strip()
 
 setuptools.setup(
     name="dislib",
