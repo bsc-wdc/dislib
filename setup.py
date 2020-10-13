11bbc0b9c16e8133337d5e6ba185f9e90b45afcf
Salvi Solà Martinell <salvi.solamartinell@bsc.es>
2020-10-13 19:04:52 +0200

    adding extra info for PyPI

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
 import setuptools
 
 
+# read the contents of the README.md file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setuptools.setup(
     name="dislib",
     version=open('VERSION').read().strip(),
     author="Barcelona Supercomputing Center",
     author_email="compss@bsc.es",
     description="The distributed computing library on top of PyCOMPSs",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url="http://dislib.bsc.es",
+    project_urls={
+        'Documentation': 'http://dislib.bsc.es',
+        'Source': 'https://github.com/bsc-wdc/dislib',
+        'Tracker': 'https://github.com/bsc-wdc/dislib/issues',
+    },
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3 :: Only",
