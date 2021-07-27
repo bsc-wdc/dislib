25f68b31a17db659cf899d63a1952e1e0eb9cccd
Guillem Casadesús Vila <gcasadesusvila@gmail.com>
2021-07-27 18:10:30 +0200

    Modified test_saving.py to raise ModuleNotFound

--- tests/test_saving.py
+++ tests/test_saving.py
@@ -1,17 +1,12 @@
 import unittest
-import sys
 import json
-
 import numpy as np
 
-# Workaround to mask cbor2
-if True:
-    sys.modules["cbor2"] = None
 from dislib.cluster import KMeans
 from dislib.cluster import DBSCAN
 from dislib.classification import RandomForestClassifier
 from dislib.data import array
-from dislib.utils.saving import save_model, load_model, _sync_obj
+import dislib.utils.saving as saving
 
 from sklearn.datasets import make_classification
 from pycompss.api.api import compss_wait_on
@@ -22,7 +17,8 @@ class SavingTest(unittest.TestCase):
 
     def test_errors(self):
         """Test that errors are raised"""
-
+        cbor2_module = saving.cbor2
+        saving.cbor2 = None
         # Models
         km = KMeans(n_clusters=2)
         km2 = KMeans(n_clusters=10)
@@ -41,51 +37,55 @@ class SavingTest(unittest.TestCase):
 
         # Import error
         with self.assertRaises(ModuleNotFoundError):
-            save_model(km, self.filepath, save_format="cbor")
+            saving.save_model(km, self.filepath, save_format="cbor")
         with self.assertRaises(ModuleNotFoundError):
-            load_model(self.filepath, load_format="cbor")
+            saving.load_model(self.filepath, load_format="cbor")
 
         # Saving model not implemented
         with self.assertRaises(NotImplementedError):
-            save_model(dbscan, self.filepath)
+            saving.save_model(dbscan, self.filepath)
 
         # Wrong save format
         with self.assertRaises(ValueError):
-            save_model(km, self.filepath, save_format="xxxx")
+            saving.save_model(km, self.filepath, save_format="xxxx")
 
         # Overwrite
-        save_model(km, self.filepath, save_format="json")
+        saving.save_model(km, self.filepath, save_format="json")
         with open(self.filepath, "r") as f:
             json_str = f.read()
-        save_model(km2, self.filepath, overwrite=False, save_format="json")
+        saving.save_model(
+            km2, self.filepath, overwrite=False, save_format="json"
+        )
         with open(self.filepath, "r") as f:
             json_str2 = f.read()
         self.assertEqual(json_str, json_str2)
 
         # Wrong load format
         with self.assertRaises(ValueError):
-            load_model(self.filepath, load_format="xxxx")
+            saving.load_model(self.filepath, load_format="xxxx")
 
         # Load model not implemented
         model_data = {"model_name": "dbscan"}
         with open(self.filepath, "w") as f:
             json.dump(model_data, f)
         with self.assertRaises(NotImplementedError):
-            load_model(self.filepath, load_format="json")
+            saving.load_model(self.filepath, load_format="json")
 
         # Not JSON serializable
         setattr(km, "n_clusters", dbscan)
         with self.assertRaises(TypeError):
-            save_model(km, self.filepath, save_format="json")
+            saving.save_model(km, self.filepath, save_format="json")
 
         # Not dict or list
         with self.assertRaises(TypeError):
-            _sync_obj(km)
+            saving._sync_obj(km)
 
         # Future not synchronized
         compss_wait_on(rf.trees[0].try_features)
         with self.assertRaises(TypeError):
-            save_model(rf, self.filepath, save_format="json")
+            saving.save_model(rf, self.filepath, save_format="json")
+
+        saving.cbor2 = cbor2_module
 
 
 def main():
