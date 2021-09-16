8d60661c7fba585578f297079246c319e478d7fd
Michal Choinski <michal.choinski@bsc.es>
2021-09-16 14:22:06 +0200

    MinMaxScaler

--- dislib/preprocessing/__init__.py
+++ dislib/preprocessing/__init__.py
@@ -1,3 +1,4 @@
-from dislib.preprocessing.classes import StandardScaler
+from dislib.preprocessing.minmax_scaler import MinMaxScaler
+from dislib.preprocessing.standard_scaler import StandardScaler
 
-__all__ = ['StandardScaler']
+__all__ = ['MinMaxScaler', 'StandardScaler']
