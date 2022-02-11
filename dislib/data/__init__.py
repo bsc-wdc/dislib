5eff35bbad0198a208345360dfa8755cb5a48638
FernandoVN98 <fernandovn98@gmail.com>
2022-02-11 14:59:23 +0100

    Added load_blocks_rechunk method

--- dislib/data/__init__.py
+++ dislib/data/__init__.py
@@ -8,4 +8,5 @@ __all__ = ['load_txt_file', 'load_svmlight_file', 'array', 'random_array',
            'apply_along_axis', 'save_npy_file', 'load_npy_file',
            'load_mdcrd_file', 'load_hstack_npy_files', 'load_npy_files',
            'load_blocks_rechunk', 'matmul', 'matsubtract', 'save_txt',
-           'zeros', 'full', 'matadd', 'concat_columns', 'identity', 'eye', 'util']
+           'zeros', 'full', 'matadd', 'concat_columns', 'identity', 'eye',
+           'util']
