6dab2da236b542068f4429c321f23e29ec5a9833
FernandoVN98 <fernandovn98@gmail.com>
2022-02-11 12:05:46 +0100

    Added tests for train_test_split method

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,6 +8,91 @@ from dislib.utils import shuffle
 
 
 class UtilsTest(unittest.TestCase):
+
+    def test_train_test_split_x(self):
+        x = np.random.rand(40, 40)
+        x_ds = ds.array(x, (8, 8))
+        train, test = ds.data.train_test_split(x_ds)
+        self.assertTrue(train.shape[0] == int(40 * 0.75))
+        self.assertTrue(test.shape[0] == int(40 * 0.25))
+        self.assertTrue(train.shape[1] == int(40 * 0.75))
+        self.assertTrue(test.shape[1] == int(40 * 0.25))
+        self.assertTrue(train._reg_shape[0] == int(8 * 0.75))
+        self.assertTrue(test._reg_shape[0] == int(8 * 0.25))
+        self.assertTrue(train._reg_shape[1] == int(8 * 0.75))
+        self.assertTrue(test._reg_shape[1] == int(8 * 0.25))
+        train = train.collect()
+        test = test.collect()
+        for idx, x_row in enumerate(x):
+            found = False
+            for train_row in train:
+                if (train_row == x_row).all():
+                    found = True
+            for test_row in test:
+                if (test_row == x_row).all():
+                    found = True
+            self.assertTrue(found)
+
+    def test_train_test_split_xy(self):
+        np.random.seed(0)
+        x = np.random.rand(40, 40)
+        y = np.random.rand(40, 1)
+        x_ds = ds.array(x, (8, 8))
+        y_ds = ds.array(y, (8, 1))
+        train, test, y_train, y_test = ds.data.train_test_split(x_ds, y_ds)
+        self.assertTrue(train.shape[0] == int(40 * 0.75))
+        self.assertTrue(test.shape[0] == int(40 * 0.25))
+        self.assertTrue(train.shape[1] == int(40 * 0.75))
+        self.assertTrue(test.shape[1] == int(40 * 0.25))
+        self.assertTrue(train._reg_shape[0] == int(8 * 0.75))
+        self.assertTrue(test._reg_shape[0] == int(8 * 0.25))
+        self.assertTrue(train._reg_shape[1] == int(8 * 0.75))
+        self.assertTrue(test._reg_shape[1] == int(8 * 0.25))
+        self.assertTrue(y_train.shape[0] == int(40 * 0.75))
+        self.assertTrue(y_test.shape[0] == int(40 * 0.25))
+        self.assertTrue(y_train.shape[1] == 1)
+        self.assertTrue(y_test.shape[1] == 1)
+        self.assertTrue(y_train._reg_shape[0] == int(8 * 0.75))
+        self.assertTrue(y_test._reg_shape[0] == int(8 * 0.25))
+        self.assertTrue(y_train._reg_shape[1] == 1)
+        self.assertTrue(y_test._reg_shape[1] == 1)
+        train = train.collect()
+        test = test.collect()
+        for idx, x_row in enumerate(x):
+            found = False
+            for train_row in train:
+                if (train_row == x_row).all():
+                    found = True
+            for test_row in test:
+                if (test_row == x_row).all():
+                    found = True
+            self.assertTrue(found)
+        y_train = y_train.collect()
+        y_test = y_test.collect()
+        for idx, x_row in enumerate(y):
+            found = False
+            for train_row in y_train:
+                if (train_row == x_row).all():
+                    found = True
+            for test_row in y_test:
+                if (test_row == x_row).all():
+                    found = True
+            self.assertTrue(found)
+
+    def test_train_test_split_excep(self):
+        x = ds.random_array((3000, 3000), (400, 400), random_state=88)
+        y = ds.random_array((3000, 1), (400, 1), random_state=88)
+        with self.assertRaises(ValueError):
+            ds.data.train_test_split(x, y, test_size=1.25, train_size=1)
+        with self.assertRaises(ValueError):
+            ds.data.train_test_split(x, y, test_size=0.95, train_size=0.1)
+        with self.assertRaises(ValueError):
+            ds.data.train_test_split(x, y=np.zeros(2, 2), test_size=0.95,
+                                     train_size=0.1)
+        with self.assertRaises(ValueError):
+            ds.data.train_test_split(x=np.zeros(2, 2), test_size=0.95,
+                                     train_size=0.1)
+
     def test_shuffle_x(self):
         """ Tests shuffle for given x and random_state. Tests that the
         shuffled array contains the same rows as the original data,
