5677df3352b586d92050a487f9035562e78070e4
FernandoVN98 <fernandovn98@gmail.com>
2022-03-24 12:26:05 +0100

    Added tests for tsqr

--- tests/test_tsqr.py
+++ tests/test_tsqr.py
@@ -5,6 +5,8 @@ import dislib as ds
 import numpy as np
 from parameterized import parameterized
 
+from dislib.decomposition.tsqr.base import _is_not_power_of_two
+
 
 class QRTest(unittest.TestCase):
     @parameterized.expand([
@@ -17,14 +19,183 @@ class QRTest(unittest.TestCase):
         m2b_ds = random_array(shape, (b_size_r, b_size_c))
 
         (q, r) = tsqr(m2b_ds)
-
+        assigned_q_shape = q.shape
+        assigned_r_shape = r.shape
         q = q.collect()
         r = r.collect()
+        m2b = m2b_ds.collect()
+        self.assertEqual(assigned_q_shape, q.shape)
+        self.assertEqual(assigned_r_shape, r.shape)
+        # check if R matrix is upper triangular
+        self.assertTrue(np.allclose(np.triu(r), r))
+        # check if Q matrix is orthogonal
+        self.assertTrue(np.allclose(np.dot(q, q.transpose()),
+                                    np.eye(q.shape[0])))
+        # check if the product Q * R is the original matrix
+        self.assertTrue(np.allclose(q.dot(r), m2b))
+
+    @parameterized.expand([
+        (2, 1, 64, 36), (4, 1, 32, 36), (16, 1, 20, 10),
+    ])
+    def test_tsqr_complete_indexes(self, m_size, n_size, b_size_r, b_size_c):
+        """Tests tsqr"""
+
+        shape = (m_size * b_size_r, n_size * b_size_c)
+        m2b_ds = random_array(shape, (b_size_r, b_size_c))
+
+        (q, r) = tsqr(m2b_ds, indexes=[2, 3, 4])
+        assigned_q_shape = q.shape
+        assigned_r_shape = r.shape
+        q = q.collect()
+        r = r.collect()
+        self.assertEqual(assigned_q_shape, q.shape)
+        self.assertEqual(assigned_r_shape, r.shape)
+        # check if R matrix is upper triangular
+        self.assertTrue(np.allclose(np.triu(r), r))
+        self.assertTrue(q.shape == (q.shape[0], 3))
+
+    @parameterized.expand([
+        (2, 1, 64, 36), (4, 1, 32, 36), (16, 1, 20, 10),
+    ])
+    def test_tsqr_inverse(self, m_size, n_size, b_size_r, b_size_c):
+        """Tests tsqr"""
+
+        shape = (m_size * b_size_r, n_size * b_size_c)
+        m2b_ds = random_array(shape, (b_size_r, b_size_c))
+
+        (q, r) = tsqr(m2b_ds, mode="complete_inverse")
+        assigned_q_shape = q.shape
+        assigned_r_shape = r.shape
+        q = q.collect()
+        r = r.collect()
+        m2b = m2b_ds.collect()
+        self.assertEqual(assigned_q_shape, q.shape)
+        self.assertEqual(assigned_r_shape, r.shape)
+        # check if R matrix is upper triangular
+        self.assertTrue(np.allclose(np.triu(r), r))
+        # check if Q matrix is orthogonal
+        self.assertTrue(np.allclose(np.dot(q, q.transpose()),
+                                    np.eye(q.shape[0])))
+        # check if the product Q * R is the original matrix
+        self.assertTrue(np.allclose(q.dot(r), m2b))
+
+    @parameterized.expand([
+        (2, 1, 64, 36), (4, 1, 32, 36), (16, 1, 20, 10),
+    ])
+    def test_tsqr_inverse_indexes(self, m_size, n_size, b_size_r, b_size_c):
+        """Tests tsqr"""
+
+        shape = (m_size * b_size_r, n_size * b_size_c)
+        m2b_ds = random_array(shape, (b_size_r, b_size_c))
+
+        (q, r) = tsqr(m2b_ds, mode="complete_inverse", indexes=[2, 3, 4])
+        assigned_q_shape = q.shape
+        assigned_r_shape = r.shape
+        q = q.collect()
+        r = r.collect()
+        self.assertEqual(assigned_q_shape, q.shape)
+        self.assertEqual(assigned_r_shape, r.shape)
+        # check if R matrix is upper triangular
+        self.assertTrue(np.allclose(np.triu(r), r))
+        self.assertTrue(q.shape==(q.shape[0], 3))
+
+    @parameterized.expand([
+        (2, 1, 64, 36), (4, 1, 32, 36), (16, 1, 20, 10),
+    ])
+    def test_tsqr_reduced(self, m_size, n_size, b_size_r, b_size_c):
+        """Tests tsqr"""
+
+        shape = (m_size * b_size_r, n_size * b_size_c)
+        m2b_ds = random_array(shape, (b_size_r, b_size_c))
+
+        (q, r) = tsqr(m2b_ds, mode="reduced")
+        assigned_q_shape = q.shape
+        assigned_r_shape = r.shape
+        q = q.collect()
+        r = r.collect()
+        self.assertEqual(assigned_q_shape, q.shape)
+        self.assertEqual(assigned_r_shape, r.shape)
         m2b = m2b_ds.collect()
         # check if R matrix is upper triangular
         self.assertTrue(np.allclose(np.triu(r), r))
         # check if the product Q * R is the original matrix
         self.assertTrue(np.allclose(q.dot(r), m2b))
+
+    @parameterized.expand([
+        (2, 1, 64, 36), (4, 1, 32, 36), (16, 1, 20, 10),
+    ])
+    def test_tsqr_reduced_inverse(self, m_size, n_size, b_size_r, b_size_c):
+        """Tests tsqr"""
+
+        shape = (m_size * b_size_r, n_size * b_size_c)
+        m2b_ds = random_array(shape, (b_size_r, b_size_c))
+
+        (q, r) = tsqr(m2b_ds, mode="reduced_inverse")
+        assigned_q_shape = q.shape
+        assigned_r_shape = r.shape
+        q = q.collect()
+        r = r.collect()
+        m2b = m2b_ds.collect()
+        self.assertEqual(assigned_q_shape, q.shape)
+        self.assertEqual(assigned_r_shape, r.shape)
+        # check if R matrix is upper triangular
+        self.assertTrue(np.allclose(np.triu(r), r))
+        # check if the product Q * R is the original matrix
+        self.assertTrue(np.allclose(q.dot(r), m2b))
+
+    @parameterized.expand([
+        (2, 1, 64, 36), (4, 1, 32, 36), (16, 1, 20, 10),
+    ])
+    def test_tsqr_reduced_inverse_indexes(self, m_size, n_size, b_size_r, b_size_c):
+        """Tests tsqr"""
+
+        shape = (m_size * b_size_r, n_size * b_size_c)
+        m2b_ds = random_array(shape, (b_size_r, b_size_c))
+
+        (q, r) = tsqr(m2b_ds, mode="reduced_inverse", indexes=[2, 3, 4])
+        assigned_q_shape = q.shape
+        assigned_r_shape = r.shape
+        q = q.collect()
+        r = r.collect()
+        self.assertEqual(assigned_q_shape, q.shape)
+        self.assertEqual(assigned_r_shape, r.shape)
+        # check if R matrix is upper triangular
+        self.assertTrue(np.allclose(np.triu(r), r))
+        # check if the product Q columns retrived are equal to the ones in the
+        # Q computed with numpy
+        self.assertTrue(q.shape==(q.shape[0], 3))
+
+    @parameterized.expand([
+        (2, 1, 64, 36), (4, 1, 32, 36), (16, 1, 20, 10),
+    ])
+    def test_tsqr_compute_r(self, m_size, n_size, b_size_r, b_size_c):
+        """Tests tsqr"""
+
+        shape = (m_size * b_size_r, n_size * b_size_c)
+        m2b_ds = random_array(shape, (b_size_r, b_size_c))
+
+        r = tsqr(m2b_ds, mode="r_complete")
+        assigned_r_shape = r.shape
+        r = r.collect()
+        self.assertEqual(assigned_r_shape, r.shape)
+        # check if R matrix is upper triangular
+        self.assertTrue(np.allclose(np.triu(r), r))
+
+    @parameterized.expand([
+        (2, 1, 64, 36), (4, 1, 32, 36), (16, 1, 20, 10),
+    ])
+    def test_tsqr_compute_r_reduced(self, m_size, n_size, b_size_r, b_size_c):
+        """Tests tsqr"""
+
+        shape = (m_size * b_size_r, n_size * b_size_c)
+        m2b_ds = random_array(shape, (b_size_r, b_size_c))
+
+        r = tsqr(m2b_ds, mode="r_reduced")
+        assigned_r_shape = r.shape
+        r = r.collect()
+        self.assertEqual(assigned_r_shape, r.shape)
+        # check if R matrix is upper triangular
+        self.assertTrue(np.allclose(np.triu(r), r))
 
     def test_tsqr_warning(self):
         m2b_ds = random_array((50, 20), (10, 10))
@@ -34,6 +205,9 @@ class QRTest(unittest.TestCase):
         m2b_ds = m2b_ds.collect()
         # check if R matrix is upper triangular
         self.assertTrue(np.allclose(np.triu(r), r))
+        # check if Q matrix is orthogonal
+        self.assertTrue(np.allclose(np.dot(q, q.transpose()),
+                                    np.eye(q.shape[0])))
         # check if the product Q * R is the original matrix
         self.assertTrue(np.allclose(q.dot(r), m2b_ds))
 
@@ -50,6 +224,26 @@ class QRTest(unittest.TestCase):
         with self.assertRaises(ValueError):
             # m > n is required for matrices m x n
             tsqr(m2b_ds)
+        m2b_ds = random_array((500, 100), (100, 100))
+        with self.assertRaises(ValueError):
+            # power of 2 is required
+            tsqr(m2b_ds, mode="complete_inverse")
+        m2b_ds = random_array((500, 100), (100, 100))
+        with self.assertRaises(ValueError):
+            # power of n_reduction is required
+            tsqr(m2b_ds, n_reduction=3, mode="complete_inverse")
+        m2b_ds = random_array((500, 100), (100, 100))
+        with self.assertRaises(ValueError):
+            # power of 2 is required
+            tsqr(m2b_ds, mode="reduced_inverse")
+        m2b_ds = random_array((500, 100), (100, 100))
+        with self.assertRaises(ValueError):
+            # power of n_reduction is required
+            tsqr(m2b_ds, n_reduction=3, mode="reduced_inverse")
+
+    def test_power_two_returns_false(self):
+        self.assertFalse(_is_not_power_of_two(0))
+        self.assertFalse(_is_not_power_of_two(-1))
 
 
 def main():
