bdf4f266cd363ac634d6d759d811099d647ac0e9
FernandoVN98 <fernandovn98@gmail.com>
2022-07-27 16:08:58 +0200

    basic neural networks used

--- dislib/eddl/models/models.py
+++ dislib/eddl/models/models.py
@@ -1,14 +1,19 @@
 import pyeddl.eddl as eddl
 
+
 def miniVGG(in_layer):
     x = in_layer
     x = eddl.BatchNormalization(eddl.ReLu(eddl.Conv(x, 32, [3, 3])))
-    x = eddl.Dropout(eddl.MaxPool(eddl.BatchNormalization(eddl.ReLu(eddl.Conv(x, 32, [3, 3]))), [2, 2], [2, 2]), 0.25)
+    x = eddl.Dropout(eddl.MaxPool(eddl.BatchNormalization(eddl.ReLu(
+        eddl.Conv(x, 32, [3, 3]))), [2, 2], [2, 2]), 0.25)
     x = eddl.BatchNormalization(eddl.ReLu(eddl.Conv(x, 64, [3, 3])))
-    x = eddl.Dropout(eddl.MaxPool(eddl.BatchNormalization(eddl.ReLu(eddl.Conv(x, 64, [3, 3]))), [2, 2], [2, 2]), 0.25)
+    x = eddl.Dropout(eddl.MaxPool(eddl.BatchNormalization(eddl.ReLu(
+        eddl.Conv(x, 64, [3, 3]))), [2, 2], [2, 2]), 0.25)
     x = eddl.Flatten(x)
-    x = eddl.Dropout(eddl.BatchNormalization(eddl.ReLu(eddl.Dense(x, 512))), 0.5)
+    x = eddl.Dropout(eddl.BatchNormalization(eddl.ReLu(eddl.Dense(x, 512))),
+                     0.5)
     return x
+
 
 def VGG19(in_layer):
     x = in_layer
@@ -34,6 +39,7 @@ def VGG19(in_layer):
     x = eddl.ReLu(eddl.Dense(x, 1000))
     return x
 
+
 def VGG16(in_layer):
     x = in_layer
     x = eddl.ReLu(eddl.Conv(x, 64, [3, 3]))
@@ -52,18 +58,4 @@ def VGG16(in_layer):
     x = eddl.Reshape(x, [-1])
     x = eddl.ReLu(eddl.Dense(x, 4096))
     x = eddl.ReLu(eddl.Dense(x, 4096))
-    #x = eddl.Softmax(eddl.Dense(x, num_classes))#TODO ESTO FUERA DE LOS MODELOS
     return x
-
-def VGG_F(in_layer):
-    x = in_layer
-    x = eddl.MaxPool(eddl.ReLu(eddl.Conv(x, 64, [3, 3])), [2, 2], [2, 2])
-    x = eddl.MaxPool(eddl.ReLu(eddl.Conv(x, 128, [3, 3])), [2, 2], [2, 2])
-    x = eddl.MaxPool(eddl.ReLu(eddl.Conv(x, 256, [3, 3])), [2, 2], [2, 2])
-    x = eddl.MaxPool(eddl.ReLu(eddl.Conv(x, 512, [3, 3])), [2, 2], [2, 2])
-    x = eddl.MaxPool(eddl.ReLu(eddl.Conv(x, 512, [3, 3])), [2, 2], [2, 2])
-    x = eddl.Reshape(x, [-1])
-    x = eddl.ReLu(eddl.Dense(x, 4096))
-    x = eddl.ReLu(eddl.Dense(x, 1000))
-    return x
-
