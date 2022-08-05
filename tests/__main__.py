361da9ddcea892088e4fad6cc788d44e8de63f56
Cristian Tatu <ctatu@bsc.es>
2022-08-05 17:06:31 +0200

    parallel tests

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,9 +1,23 @@
 import unittest
+import threading
 
 
-def load_tests(loader, tests, pattern):
+def load_tests(loader):
     return loader.discover('./tests/')
 
 
+def run_test(test):
+    suite = unittest.TestSuite()
+    suite.addTest(test)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+
 if __name__ == '__main__':
-    unittest.main(verbosity=2)
+    suite = load_tests(unittest.loader.defaultTestLoader)
+    threads = []
+    for case in suite:
+        thread = threading.Thread(target=run_test, args=(case,))
+        thread.start()
+        threads.append(thread)
+
+    [t.join() for t in threads]
