d5a5fa35473f4548f862e84e4b8c7d82447ca683
Cristian Tatu <ctatu@bsc.es>
2022-08-08 12:08:20 +0200

    Update __main__.py

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,23 +1,32 @@
 import unittest
-import threading
+import multiprocessing as mp
+from concurrent.futures import ThreadPoolExecutor
 
+T = [["test_lasso"],
+     ["test_array", "test_pca", "test_daura"],
+     ["test_gm", "test_preproc", "test_decision_tree"],
+     ["test_qr", "test_kmeans", "test_knn"],
+     ["test_gridsearch", "test_tsqr", "test_linear_regression"],
+     ["test_dbscan", "test_matmul", "test_als"],
+     ["test_rf_classifier", "test_randomizedsearch", "test_data_utils", "test_kfold"],
+     ["test_csvm", "test_rf_regressor", "test_utils", "test_rf_dataset"]]
 
-def load_tests(loader):
-    return loader.discover('./tests/')
-
-
-def run_test(test):
-    suite = unittest.TestSuite()
-    suite.addTest(test)
-    unittest.TextTestRunner(verbosity=2).run(suite)
-
+def run_tests(tests_to_run):
+    test_suite = unittest.TestSuite()
+    test_suite.addTests(tests_to_run)
+    unittest.TextTestRunner(verbosity=2).run(test_suite)
 
 if __name__ == '__main__':
-    suite = load_tests(unittest.loader.defaultTestLoader)
-    threads = []
-    for case in suite:
-        thread = threading.Thread(target=run_test, args=(case,))
-        thread.start()
-        threads.append(thread)
+    suite = list(unittest.loader.defaultTestLoader.discover('./tests/'))
 
-    [t.join() for t in threads]
+    tests_to_run = []
+    for tests in T:
+        ttt = []
+        for t in tests:
+            for test_case in suite:
+                if t.lower() in str(test_case).lower():
+                    ttt.append(test_case)
+        tests_to_run.append(ttt)
+                
+    with ThreadPoolExecutor(max_workers=mp.cpu_count) as exec:
+        exec.map(run_tests, tests_to_run)
