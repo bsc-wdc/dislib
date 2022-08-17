8286d8ff95a9cd32a05c223f7f63ee5adc0ef98e
Cristian Tatu <ctatu@bsc.es>
2022-08-17 23:23:50 +0200

    aa

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,12 +1,15 @@
+import datetime
 from time import time
 import unittest
 
 
 class BaseTimedTestCase(unittest.TestCase):
     def setUp(self):
-        self.start_time = time()
+        # self.start_time = time()
+        print(f'Test {self.id()} START AT', datetime.datetime.now(), flush=True)
 
     def tearDown(self):
-        self.end_time = time()
-        print("Test %s took: %.3f seconds" %
-              (self.id(), self.end_time - self.start_time))
+        print(f'Test {self.id()} END AT', datetime.datetime.now(), flush=True)
+        # self.end_time = time()
+        # print("Test %s took: %.3f seconds" %
+        #       (self.id(), self.end_time - self.start_time))
