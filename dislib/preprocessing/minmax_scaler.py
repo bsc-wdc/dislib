621b5e5dd0ecb964131f3a1976d24b8c36a77b27
Fernando Vázquez <fernandovn98@gmail.com>
2023-03-08 10:18:45 +0100

    Added save and load methods

--- dislib/preprocessing/minmax_scaler.py
+++ dislib/preprocessing/minmax_scaler.py
@@ -1,3 +1,7 @@
+import json
+import os
+import pickle
+
 import numpy as np
 from pycompss.api.constraint import constraint
 from pycompss.api.parameter import Depth, Type, COLLECTION_IN, COLLECTION_OUT
@@ -6,6 +10,9 @@ from scipy.sparse import csr_matrix, issparse
 
 from dislib.data.array import Array
 import dislib as ds
+
+import dislib.data.util.model as utilmodel
+from dislib.data.util import encoder_helper, decoder_helper, sync_obj
 
 
 class MinMaxScaler(object):
@@ -119,6 +126,142 @@ class MinMaxScaler(object):
         return Array(blocks, top_left_shape=x._top_left_shape,
                      reg_shape=x._reg_shape, shape=x.shape,
                      sparse=x._sparse)
+
+    def save_model(self, filepath, overwrite=True, save_format="json"):
+        """Saves a model to a file.
+        The model is synchronized before saving and can be reinstantiated in
+        the exact same state, without any of the code used for model
+        definition or fitting.
+        Parameters
+        ----------
+        filepath : str
+            Path where to save the model
+        overwrite : bool, optional (default=True)
+            Whether any existing model at the target
+            location should be overwritten.
+        save_format : str, optional (default='json)
+            Format used to save the models.
+        Examples
+        --------
+        >>> from dislib.preprocessing import MinMaxScaler
+        >>> import numpy as np
+        >>> import dislib as ds
+        >>> x = ds.array(np.array([[1, 2], [2, 1], [-1, -2],
+        >>> [-2, -1]]), (2, 2))
+        >>> y = ds.array(np.array([0, 1, 1, 0]).reshape(-1, 1), (2, 1))
+        >>> model = MinMaxScaler()
+        >>> model.fit(x)
+        >>> model.save_model('/tmp/model')
+        >>> loaded_model = MinMaxScaler()
+        >>> loaded_model.load_model('/tmp/model')
+        >>> x_test = ds.array(np.array([[1, 2], [2, 1], [-1, -2], [-2, -1],
+        >>> [1, 1], [-1, -1]]), (2, 2))
+        >>> x_transformed = model.transform(x_test)
+        >>> x_loaded_pred = loaded_model.transform(x_test)
+        >>> assert np.allclose(x_transformed.collect(),
+        >>> x_loaded_pred.collect())
+        """
+
+        # Check overwrite
+        if not overwrite and os.path.isfile(filepath):
+            return
+
+        sync_obj(self.__dict__)
+        model_metadata = self.__dict__
+        model_metadata["model_name"] = "minmaxscaler"
+
+        # Save model
+        if save_format == "json":
+            with open(filepath, "w") as f:
+                json.dump(model_metadata, f, default=_encode_helper)
+        elif save_format == "cbor":
+            if utilmodel.cbor2 is None:
+                raise ModuleNotFoundError("No module named 'cbor2'")
+            with open(filepath, "wb") as f:
+                utilmodel.cbor2.dump(model_metadata, f,
+                                     default=_encode_helper_cbor)
+        elif save_format == "pickle":
+            with open(filepath, "wb") as f:
+                pickle.dump(model_metadata, f)
+        else:
+            raise ValueError("Wrong save format.")
+
+    def load_model(self, filepath, load_format="json"):
+        """Loads a model from a file.
+        The model is reinstantiated in the exact same state in which it was
+        saved, without any of the code used for model definition or fitting.
+        Parameters
+        ----------
+        filepath : str
+            Path of the saved the model
+        load_format : str, optional (default='json')
+            Format used to load the model.
+        Examples
+        --------
+        >>> from dislib.classification import CascadeSVM
+        >>> import numpy as np
+        >>> import dislib as ds
+        >>> x = ds.array(np.array([[1, 2], [2, 1], [-1, -2],
+        >>> [-2, -1]]), (2, 2))
+        >>> y = ds.array(np.array([0, 1, 1, 0]).reshape(-1, 1), (2, 1))
+        >>> model = MinMaxScaler()
+        >>> model.fit(x)
+        >>> model.save_model('/tmp/model')
+        >>> loaded_model = MinMaxScaler()
+        >>> loaded_model.load_model('/tmp/model')
+        >>> x_test = ds.array(np.array([[1, 2], [2, 1], [-1, -2], [-2, -1],
+        >>> [1, 1], [-1, -1]]), (2, 2))
+        >>> x_transformed = model.transform(x_test)
+        >>> x_loaded_pred = loaded_model.transform(x_test)
+        >>> assert np.allclose(x_transformed.collect(),
+        >>>                    x_loaded_pred.collect())
+        """
+        # Load model
+        if load_format == "json":
+            with open(filepath, "r") as f:
+                model_metadata = json.load(f, object_hook=_decode_helper)
+        elif load_format == "cbor":
+            if utilmodel.cbor2 is None:
+                raise ModuleNotFoundError("No module named 'cbor2'")
+            with open(filepath, "rb") as f:
+                model_metadata = utilmodel.cbor2.\
+                    load(f, object_hook=_decode_helper_cbor)
+        elif load_format == "pickle":
+            with open(filepath, "rb") as f:
+                model_metadata = pickle.load(f)
+        else:
+            raise ValueError("Wrong load format.")
+
+        for key, val in model_metadata.items():
+            setattr(self, key, val)
+
+
+def _encode_helper_cbor(encoder, obj):
+    encoder.encode(_encode_helper(obj))
+
+
+def _encode_helper(obj):
+    encoded = encoder_helper(obj)
+    if encoded is not None:
+        return encoded
+
+
+def _decode_helper_cbor(decoder, obj):
+    """Special decoder wrapper for dislib using cbor2."""
+    return _decode_helper(obj)
+
+
+def _decode_helper(obj):
+    if isinstance(obj, dict) and "class_name" in obj:
+        class_name = obj["class_name"]
+        decoded = decoder_helper(class_name, obj)
+        if decoded is not None:
+            return decoded
+        elif class_name == "RandomState":
+            random_state = np.random.RandomState()
+            random_state.set_state(_decode_helper(obj["items"]))
+            return random_state
+    return obj
 
 
 @constraint(computing_units="${ComputingUnits}")
