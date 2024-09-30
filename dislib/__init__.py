9773807ec664772ecd34e38f97b51839f144b0e3
fvazquez <fernando.vazquez@bsc.es>
2024-09-30 11:15:34 +0200

    Corrected style

--- dislib/__init__.py
+++ dislib/__init__.py
@@ -4,21 +4,11 @@ from dislib.data.array import random_array, apply_along_axis, array, zeros, \
     full, identity, eye, matmul, concat_rows, concat_columns, matadd, \
     matsubtract
 try:
-    from dislib.data.tensor import random_tensors, from_array, from_pt_tensor, \
-        create_ds_tensor
-    methods = ['array', 'random_array', 'zeros', 'full', 'identity', 'eye',
-               'load_txt_file', 'load_svmlight_file', 'load_npy_file',
-               'load_mdcrd_file', 'matmul', 'matadd', 'matsubtract',
-               'random_tensors', 'from_array', 'from_pt_tensor',
-               'create_ds_tensor', 'save_txt', 'concat_rows',
-               'concat_columns', 'apply_along_axis', 'kron', 'svd']
-except Exception as e:
+    from dislib.data.tensor import random_tensors, from_array, \
+        from_pt_tensor, create_ds_tensor
+    imported_tensors = True
+except Exception:
     print("WARNING: Tensors have not been loaded. No module named 'torch'.")
-    methods = ['array', 'random_array', 'zeros', 'full', 'identity', 'eye',
-               'load_txt_file', 'load_svmlight_file', 'load_npy_file',
-               'load_mdcrd_file', 'matmul', 'matadd', 'matsubtract',
-               'save_txt', 'concat_rows', 'concat_columns',
-               'apply_along_axis', 'kron', 'svd']
 from dislib.data.io import load_svmlight_file, load_npy_file, load_txt_file, \
     load_mdcrd_file, save_txt
 from dislib.math import kron, svd
@@ -45,7 +35,13 @@ else:
               "Probably it was not installed with setup.py.\n%s" % e)
         __version__ = 'unknown'
 
-__all__ = methods
+__all__ = ['array', 'random_array', 'zeros', 'full', 'identity', 'eye',
+           'load_txt_file', 'load_svmlight_file', 'load_npy_file',
+           'load_mdcrd_file', 'matmul', 'matadd', 'matsubtract',
+           'random_tensors', 'from_array', 'from_pt_tensor',
+           'create_ds_tensor',
+           'save_txt', 'concat_rows', 'concat_columns',
+           'apply_along_axis', 'kron', 'svd']
 
 gpu_envar = os.environ.get('DISLIB_GPU_AVAILABLE', 'False')
 __gpu_available__ = gpu_envar.lower() == 'true'
