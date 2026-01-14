5c63470b13242c7c537af4cc4c808e5d772a098c
iraola <20189817+iraola@users.noreply.github.com>
2026-01-14 10:14:42 +0100

    Substitute deprecated parameterized by subTest
    Replace usage of numpy.testing._private.parameterized with
    unittest.TestCase.subTest. This removes reliance on NumPy private
    APIs that were removed in v2.x, while preserving test coverage
    across multiple parameter configurations.

--- tests/test_preproc.py
+++ tests/test_preproc.py
@@ -1,7 +1,6 @@
 import unittest
 
 import numpy as np
-from numpy.testing._private.parameterized import parameterized
 from pycompss.api.api import compss_wait_on
 from scipy.sparse import csr_matrix, issparse
 from sklearn.datasets import make_blobs
@@ -26,143 +25,148 @@ class ScalerTest(BaseTimedTestCase):
 
 class MinMaxScalerTest(ScalerTest):
 
-    @parameterized.expand([((0, 1),),
-                           ((-1, 1),)])
-    def test_fit_transform(self, feature_range):
+    def test_fit_transform(self):
         """ Tests fit_transform against scikit-learn.
         """
+        for feature_range in [(0, 1), (-1, 1)]:
+            with self.subTest(feature_range=feature_range):
+                sc1 = SkMinMaxScaler(feature_range=feature_range)
+                scaled_x = sc1.fit_transform(self.x)
+                sc2 = MinMaxScaler(feature_range=feature_range)
+                ds_scaled = sc2.fit_transform(self.ds_arr)
 
-        sc1 = SkMinMaxScaler(feature_range=feature_range)
-        scaled_x = sc1.fit_transform(self.x)
-        sc2 = MinMaxScaler(feature_range=feature_range)
-        ds_scaled = sc2.fit_transform(self.ds_arr)
+                self.assertTrue(np.allclose(scaled_x, ds_scaled.collect()))
+                self.assertTrue(np.allclose(sc1.data_min_,
+                                            sc2.data_min_.collect()))
+                self.assertTrue(np.allclose(sc1.data_max_,
+                                            sc2.data_max_.collect()))
+                self.assertEqual(ds_scaled._top_left_shape,
+                                 ds_scaled._blocks[0][0].shape)
+                self.assertEqual(self.ds_arr._reg_shape, ds_scaled._reg_shape)
+                self.assertEqual(self.ds_arr._top_left_shape,
+                                 ds_scaled._top_left_shape)
+                self.assertEqual(self.ds_arr.shape, ds_scaled.shape)
+                self.assertEqual(self.ds_arr._n_blocks, ds_scaled._n_blocks)
 
-        self.assertTrue(np.allclose(scaled_x, ds_scaled.collect()))
-        self.assertTrue(np.allclose(sc1.data_min_, sc2.data_min_.collect()))
-        self.assertTrue(np.allclose(sc1.data_max_, sc2.data_max_.collect()))
-        self.assertEqual(ds_scaled._top_left_shape,
-                         ds_scaled._blocks[0][0].shape)
-        self.assertEqual(self.ds_arr._reg_shape, ds_scaled._reg_shape)
-        self.assertEqual(self.ds_arr._top_left_shape,
-                         ds_scaled._top_left_shape)
-        self.assertEqual(self.ds_arr.shape, ds_scaled.shape)
-        self.assertEqual(self.ds_arr._n_blocks, ds_scaled._n_blocks)
-
-    @parameterized.expand([((0, 1),),
-                           ((-1, 1),)])
-    def test_inverse_transform(self, feature_range):
+    def test_inverse_transform(self):
         """ Tests inverse_transform against scikit-learn.
         """
-        n_samples = 500
-        x, y = make_blobs(n_samples=n_samples, random_state=170)
-        transformation = [[0.6, -0.6], [-0.4, 0.8]]
-        x = np.dot(x, transformation)
-        ds_arr = ds.array(x, block_size=(300, 2))
+        for feature_range in [(0, 1), (-1, 1)]:
+            with self.subTest(feature_range=feature_range):
+                n_samples = 500
+                x, y = make_blobs(n_samples=n_samples, random_state=170)
+                transformation = [[0.6, -0.6], [-0.4, 0.8]]
+                x = np.dot(x, transformation)
+                ds_arr = ds.array(x, block_size=(300, 2))
 
-        sc1 = SkMinMaxScaler(feature_range=feature_range)
-        scaled_x = sc1.fit_transform(x)
-        scaled_x = sc1.inverse_transform(scaled_x)
-        sc2 = MinMaxScaler(feature_range=feature_range)
-        ds_scaled = sc2.fit_transform(ds_arr)
-        ds_scaled = sc2.inverse_transform(ds_scaled)
+                sc1 = SkMinMaxScaler(feature_range=feature_range)
+                scaled_x = sc1.fit_transform(x)
+                scaled_x = sc1.inverse_transform(scaled_x)
+                sc2 = MinMaxScaler(feature_range=feature_range)
+                ds_scaled = sc2.fit_transform(ds_arr)
+                ds_scaled = sc2.inverse_transform(ds_scaled)
 
-        self.assertTrue(np.allclose(scaled_x, ds_scaled.collect()))
-        self.assertTrue(np.allclose(sc1.data_min_, sc2.data_min_.collect()))
-        self.assertTrue(np.allclose(sc1.data_max_, sc2.data_max_.collect()))
-        self.assertEqual(ds_scaled._top_left_shape,
-                         ds_scaled._blocks[0][0].shape)
-        self.assertEqual(ds_arr._reg_shape, ds_scaled._reg_shape)
-        self.assertEqual(ds_arr._top_left_shape, ds_scaled._top_left_shape)
-        self.assertEqual(ds_arr.shape, ds_scaled.shape)
-        self.assertEqual(ds_arr._n_blocks, ds_scaled._n_blocks)
+                self.assertTrue(np.allclose(scaled_x, ds_scaled.collect()))
+                self.assertTrue(np.allclose(sc1.data_min_,
+                                            sc2.data_min_.collect()))
+                self.assertTrue(np.allclose(sc1.data_max_,
+                                            sc2.data_max_.collect()))
+                self.assertEqual(ds_scaled._top_left_shape,
+                                 ds_scaled._blocks[0][0].shape)
+                self.assertEqual(ds_arr._reg_shape, ds_scaled._reg_shape)
+                self.assertEqual(ds_arr._top_left_shape,
+                                 ds_scaled._top_left_shape)
+                self.assertEqual(ds_arr.shape, ds_scaled.shape)
+                self.assertEqual(ds_arr._n_blocks, ds_scaled._n_blocks)
 
-    @parameterized.expand([((0, 1),),
-                           ((-1, 1),)])
-    def test_sparse(self, feature_range):
+    def test_sparse(self):
         """ Tests fit_transforms with sparse data"""
+        for feature_range in [(0, 1), (-1, 1)]:
+            with self.subTest(feature_range=feature_range):
+                sc = MinMaxScaler(feature_range=feature_range)
+                dense_scaled = sc.fit_transform(self.ds_arr)
+                dense_min = sc.data_min_.collect()
+                dense_max = sc.data_max_.collect()
 
-        sc = MinMaxScaler(feature_range=feature_range)
-        dense_scaled = sc.fit_transform(self.ds_arr)
-        dense_min = sc.data_min_.collect()
-        dense_max = sc.data_max_.collect()
+                sparse_scaled = sc.fit_transform(self.sparse_arr)
+                sparse_min = sc.data_min_.collect()
+                sparse_max = sc.data_max_.collect()
 
-        sparse_scaled = sc.fit_transform(self.sparse_arr)
-        sparse_min = sc.data_min_.collect()
-        sparse_max = sc.data_max_.collect()
+                csr_scaled = sparse_scaled.collect()
+                arr_scaled = dense_scaled.collect()
 
-        csr_scaled = sparse_scaled.collect()
-        arr_scaled = dense_scaled.collect()
+                self.assertTrue(issparse(csr_scaled))
+                self.assertTrue(sparse_scaled._sparse)
+                self.assertTrue(sc.data_min_._sparse)
+                self.assertTrue(sc.data_max_._sparse)
+                self.assertTrue(issparse(sparse_min))
+                self.assertTrue(issparse(sparse_max))
 
-        self.assertTrue(issparse(csr_scaled))
-        self.assertTrue(sparse_scaled._sparse)
-        self.assertTrue(sc.data_min_._sparse)
-        self.assertTrue(sc.data_max_._sparse)
-        self.assertTrue(issparse(sparse_min))
-        self.assertTrue(issparse(sparse_max))
+                self.assertTrue(np.allclose(csr_scaled.toarray(), arr_scaled))
+                self.assertTrue(np.allclose(sparse_min.toarray(), dense_min))
+                self.assertTrue(np.allclose(sparse_max.toarray(), dense_max))
 
-        self.assertTrue(np.allclose(csr_scaled.toarray(), arr_scaled))
-        self.assertTrue(np.allclose(sparse_min.toarray(), dense_min))
-        self.assertTrue(np.allclose(sparse_max.toarray(), dense_max))
-
-    @parameterized.expand([((0, 1),),
-                           ((-1, 1),)])
-    def test_sparse_inverse_transform(self, feature_range):
+    def test_sparse_inverse_transform(self):
         """ Tests inverse_transform with sparse data"""
-        n_samples = 500
-        x, y = make_blobs(n_samples=n_samples, random_state=170)
-        transformation = [[0.6, -0.6], [-0.4, 0.8]]
-        x = np.dot(x, transformation)
+        for feature_range in [(0, 1), (-1, 1)]:
+            with self.subTest(feature_range=feature_range):
+                n_samples = 500
+                x, y = make_blobs(n_samples=n_samples, random_state=170)
+                transformation = [[0.6, -0.6], [-0.4, 0.8]]
+                x = np.dot(x, transformation)
 
-        dense_arr = ds.array(x, block_size=(300, 2))
-        sparse_arr = ds.array(csr_matrix(x), block_size=(300, 2))
+                dense_arr = ds.array(x, block_size=(300, 2))
+                sparse_arr = ds.array(csr_matrix(x), block_size=(300, 2))
 
-        sc = MinMaxScaler(feature_range=feature_range)
-        dense_scaled = sc.fit_transform(dense_arr)
-        dense_scaled = sc.inverse_transform(dense_scaled)
-        dense_min = sc.data_min_.collect()
-        dense_max = sc.data_max_.collect()
+                sc = MinMaxScaler(feature_range=feature_range)
+                dense_scaled = sc.fit_transform(dense_arr)
+                dense_scaled = sc.inverse_transform(dense_scaled)
+                dense_min = sc.data_min_.collect()
+                dense_max = sc.data_max_.collect()
 
-        sparse_scaled = sc.fit_transform(sparse_arr)
-        sparse_scaled = sc.inverse_transform(sparse_scaled)
-        sparse_min = sc.data_min_.collect()
-        sparse_max = sc.data_max_.collect()
+                sparse_scaled = sc.fit_transform(sparse_arr)
+                sparse_scaled = sc.inverse_transform(sparse_scaled)
+                sparse_min = sc.data_min_.collect()
+                sparse_max = sc.data_max_.collect()
 
-        csr_scaled = sparse_scaled.collect()
-        arr_scaled = dense_scaled.collect()
+                csr_scaled = sparse_scaled.collect()
+                arr_scaled = dense_scaled.collect()
 
-        self.assertTrue(issparse(csr_scaled))
-        self.assertTrue(sparse_scaled._sparse)
-        self.assertTrue(sc.data_min_._sparse)
-        self.assertTrue(sc.data_max_._sparse)
-        self.assertTrue(issparse(sparse_min))
-        self.assertTrue(issparse(sparse_max))
+                self.assertTrue(issparse(csr_scaled))
+                self.assertTrue(sparse_scaled._sparse)
+                self.assertTrue(sc.data_min_._sparse)
+                self.assertTrue(sc.data_max_._sparse)
+                self.assertTrue(issparse(sparse_min))
+                self.assertTrue(issparse(sparse_max))
 
-        self.assertTrue(np.allclose(csr_scaled.toarray(), arr_scaled))
-        self.assertTrue(np.allclose(sparse_min.toarray(), dense_min))
-        self.assertTrue(np.allclose(sparse_max.toarray(), dense_max))
+                self.assertTrue(np.allclose(csr_scaled.toarray(), arr_scaled))
+                self.assertTrue(np.allclose(sparse_min.toarray(), dense_min))
+                self.assertTrue(np.allclose(sparse_max.toarray(), dense_max))
 
-    @parameterized.expand([((0, 1),),
-                           ((-1, 1),)])
-    def test_irregular(self, feature_range):
+    def test_irregular(self):
         """ Test with an irregular array """
+        for feature_range in [(0, 1), (-1, 1)]:
+            with self.subTest(feature_range=feature_range):
+                ds_arr = self.ds_arr[297:602]
+                x = self.x[297:602]
 
-        ds_arr = self.ds_arr[297:602]
-        x = self.x[297:602]
+                sc1 = SkMinMaxScaler(feature_range=feature_range)
+                scaled_x = sc1.fit_transform(x)
+                sc2 = MinMaxScaler(feature_range=feature_range)
+                ds_scaled = sc2.fit_transform(ds_arr)
 
-        sc1 = SkMinMaxScaler(feature_range=feature_range)
-        scaled_x = sc1.fit_transform(x)
-        sc2 = MinMaxScaler(feature_range=feature_range)
-        ds_scaled = sc2.fit_transform(ds_arr)
-
-        self.assertTrue(np.allclose(scaled_x, ds_scaled.collect()))
-        self.assertTrue(np.allclose(sc1.data_min_, sc2.data_min_.collect()))
-        self.assertTrue(np.allclose(sc1.data_max_, sc2.data_max_.collect()))
-        self.assertEqual(ds_scaled._top_left_shape,
-                         compss_wait_on(ds_scaled._blocks[0][0]).shape)
-        self.assertEqual(ds_arr._reg_shape, ds_scaled._reg_shape)
-        self.assertEqual(ds_arr._top_left_shape, ds_scaled._top_left_shape)
-        self.assertEqual(ds_arr.shape, ds_scaled.shape)
-        self.assertEqual(ds_arr._n_blocks, ds_scaled._n_blocks)
+                self.assertTrue(np.allclose(scaled_x, ds_scaled.collect()))
+                self.assertTrue(np.allclose(sc1.data_min_,
+                                            sc2.data_min_.collect()))
+                self.assertTrue(np.allclose(sc1.data_max_,
+                                            sc2.data_max_.collect()))
+                self.assertEqual(ds_scaled._top_left_shape,
+                                 compss_wait_on(ds_scaled._blocks[0][0]).shape)
+                self.assertEqual(ds_arr._reg_shape, ds_scaled._reg_shape)
+                self.assertEqual(ds_arr._top_left_shape,
+                                 ds_scaled._top_left_shape)
+                self.assertEqual(ds_arr.shape, ds_scaled.shape)
+                self.assertEqual(ds_arr._n_blocks, ds_scaled._n_blocks)
 
     def test_save_load(self):
         """
